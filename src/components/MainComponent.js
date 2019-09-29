c77cb1aaa9b1ecf22f53315e8c0f6fb6bbef83f8
pea-sys <pea98258@gmail.com>
2019-09-29 10:49:32 +0900

    Assignment 2: React Router and Single Page Applications

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishDetailComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -48,6 +49,7 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
               <Route exact path='/contactus' component={Contact}/>
               <Redirect to="/home" />
           </Switch>
