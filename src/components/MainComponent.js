f446ed4eccd8953bb85bda3b9c4be20bd246060e
pea-sys <pea98258@gmail.com>
2019-10-05 08:14:06 +0900

    Exercise Reduxx Thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,12 +13,14 @@ import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 
 const mapDispatchToProps = dispatch => ({
   
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes())}
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 });
 
 const mapStateToProps = state => {
@@ -72,7 +74,7 @@ class Main extends Component {
           <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
-          <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />/>
           <Redirect to="/home" />
         </Switch>
 
